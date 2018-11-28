763af791fffc811156f10b0be0db491906cdb2ad
mariette souppe <mariette@eduroam-169-233-180-184.ucsc.edu>
2018-11-28 15:06:06 -0800

    Clean up main and util functions

---
+++ util.R
@@ -0,0 +1,84 @@
+## File description: 
+
+# Obtain response from dataset
+response <- function(dataset) {
+  resp <- dataset$winPlacePerc
+  return(resp)
+}
+
+# Take out any data fields that will not be used for data analysis
+omit_data_fields <- function(dataset, matchtype) {
+  matchtype_data <- dataset[which(dataset$matchType==matchtype),]
+  
+  if (matchtype == "solo" | matchtype == "solo-fpp") {
+    omit_categories <- c("winPlacePerc", "winPoints","killPoints","Id","groupId",
+                     "matchId","matchType","DBNOs","revives","rankPoints")    
+  } else {
+    omit_categories <- c("winPlacePerc", "winPoints","killPoints","Id","groupId",
+                     "matchId","matchType", "rankPoints") 
+  }
+  
+  subset_matchtype_data <- matchtype_data[ , !(names(matchtype_data) %in% omit_categories)]
+  return(subset_matchtype_data)
+}
+
+# Subset data into map sizes and response
+preprocess <- function(dataset, matchtype) { 
+  # Map sizes; large and small 
+  large_map <- dataset$matchDuration >= 1600
+  small_map <- dataset$matchDuration < 1600
+  
+  # Split whole dataset into two different map sizes
+  large_map_dataset <- dataset[which(large_map),]
+  small_map_dataset <- dataset[which(small_map),]
+  
+  # Subset map data by match type
+  matchtype_large_map_dataset <- large_map_dataset[which(large_map_dataset$matchType==matchtype),]
+  matchtype_small_map_dataset <- small_map_dataset[which(small_map_dataset$matchType==matchtype),]
+  
+  # Get response
+  large_map_response <- response(matchtype_large_map_dataset)
+  small_map_response <- response(matchtype_small_map_dataset)
+  
+  # Omit data fields that won't be used for analyzing
+  mt_large_map_dataset <- omit_data_fields(large_map_dataset, matchtype)
+  mt_small_map_dataset <- omit_data_fields(small_map_dataset, matchtype)
+
+  data_out <- list(first=mt_large_map_dataset, second=mt_small_map_dataset, third=large_map_response, fourth=small_map_response)
+  return(data_out)
+}
+
+# Find the best k-means
+min_k_means <- function(df,num_times,num_clusters) {
+  set.seed(34529078)
+  best_fit <- kmeans(df, num_clusters)
+  best_fit_score <- best_fit$betweenss/best_fit$totss
+  for (i in 1:num_times) {
+    candidate_fit <- kmeans(df, num_clusters)
+    candidate_fit_score <- candidate_fit$betweenss/candidate_fit$totss
+    if (candidate_fit_score >  best_fit_score) {
+      best_fit <- candidate_fit
+      best_fit_score <- candidate_fit_score
+    }
+  }
+  return(best_fit)
+}
+
+# Add cluster feature to the dataset
+append_clustering_feature <- function(covariate_type_frame, full_type_frame) {
+  # Mutate Data Frame do not return a new full_type_frame
+  k_means_results <- min_k_means(covariate_type_frame,30,4)
+  cluster <- k_means_results$cluster
+  new_df <- data.frame(full_type_frame,cluster)
+  return(new_df)
+}
+
+# Split cluster
+split_by_the_four_clusters <- function(full_type_frame) {
+  # For now only assuming that we are using four clusters
+  cluster_1_df <- full_type_frame[which(full_type_frame$cluster == 1), ] 
+  cluster_2_df <- full_type_frame[which(full_type_frame$cluster == 2), ] 
+  cluster_3_df <- full_type_frame[which(full_type_frame$cluster == 3), ]
+  cluster_4_df <- full_type_frame[which(full_type_frame$cluster == 4), ] 
+  return(list(first = cluster_1_df,second = cluster_2_df,third = cluster_3_df,fourth = cluster_4_df))
+}
